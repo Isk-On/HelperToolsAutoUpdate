de982f2be40add66292da81bccacb62b400ee56f
Isk-On <119677180+Isk-On@users.noreply.github.com>
2023-10-21 17:52:25 +0500

    Add files via upload

---
+++ autoupdate.lua
@@ -0,0 +1,60 @@
+local dlstatus = require('moonloader').download_status
+local update_state = false -- Если переменная == true, значит начнётся обновление.
+local update_found = false -- Если будет true, будет доступна команда /update.
+
+local script_vers = 2.0
+local script_vers_text = "v1.0" -- Название нашей версии. В будущем будем её выводить ползователю.
+
+local update_url = ''           -- Путь к ini файлу. Позже нам понадобиться.
+local update_path = getWorkingDirectory() .. "/update.ini"
+
+local script_url = '' -- Путь скрипту.
+local script_path = thisScript().path
+
+
+
+function check_update() -- Создаём функцию которая будет проверять наличие обновлений при запуске скрипта.
+    downloadUrlToFile(update_url, update_path, function(id, status)
+        if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+            updateIni = inicfg.load(nil, update_path)
+            if tonumber(updateIni.info.vers) > script_vers then -- Сверяем версию в скрипте и в ini файле на github
+                sampAddChatMessage(
+                    "{FFFFFF}Имеется {32CD32}новая {FFFFFF}версия скрипта. Версия: {32CD32}" ..
+                    updateIni.info.vers_text .. ". {FFFFFF}/update что-бы обновить", 0xFF0000) -- Сообщаем о новой версии.
+                update_found = true -- если обновление найдено, ставим переменной значение true
+            end
+            os.remove(update_path)
+        end
+    end)
+end
+
+function main()
+    while not isSampAvailable() do wait(100) end
+
+    check_update()
+
+    sampRegisterChatCommand('test', function()
+        sampAddChatMessage("в этом обновлении добавили команду /test, ваааацууууууу", -1) 
+    end)
+
+    if update_found then                             -- Если найдено обновление, регистрируем команду /update.
+        sampRegisterChatCommand('update', function() 
+            update_state = true                      
+        end)
+    else
+        sampAddChatMessage('{FFFFFF}Нету доступных обновлений!')
+    end
+
+    while true do
+        wait(0)
+
+        if update_state then -- Если человек напишет /update и обновлени есть, начнётся скаачивание скрипта.
+            downloadUrlToFile(script_url, script_path, function(id, status)
+                if status == dlstatus.STATUS_ENDDOWNLOADDATA then
+                    sampAddChatMessage("{FFFFFF}Скрипт {32CD32}успешно {FFFFFF}обновлён.", 0xFF0000)
+                end
+            end)
+            break
+        end
+    end
+end
